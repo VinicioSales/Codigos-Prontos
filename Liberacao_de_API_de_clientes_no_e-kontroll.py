c6206e516016f635210445d5fe95593af388c98f
VinicioSales <vinicio.santtana@gmail.com>
2022-07-13 11:00:47 -0300

    att

--- Liberacao_de_API_de_clientes_no_e-kontroll.py
+++ Liberacao_de_API_de_clientes_no_e-kontroll.py
@@ -23,12 +23,13 @@ navegador.maximize_window()
 time.sleep(5)
 
 #Logando
-navegador.find_element(By.XPATH,'/html/body/div/div/section/form/div[1]/input').send_keys('')
+navegador.find_element(By.XPATH,'/html/body/div/div/section/form/div[1]/input').send_keys('consultoria.britoesantana')
 time.sleep(1)
-navegador.find_element(By.XPATH,'/html/body/div/div/section/form/div[2]/input').send_keys('')
+navegador.find_element(By.XPATH,'/html/body/div/div/section/form/div[2]/input').send_keys('parceiro2021')
 time.sleep(1)
 navegador.find_element(By.XPATH,'/html/body/div/div/section/form/div[3]/button').click()
 time.sleep(5)
+
 navegador.find_element(By.XPATH,'//*[@id="dlgModalFeira"]/div/div/div/div/button').click()
 time.sleep(1)
 navegador.find_element(By.XPATH,'//*[@id="sidebar-menu"]/div/ul/li[6]/a/nobr').click()
@@ -36,22 +37,25 @@ time.sleep(3)
 navegador.find_element(By.XPATH,'/html/body/div[1]/div/div[3]/div/nav/ul/li[1]/a').click()
 time.sleep(5)
 
+#Ativando API
 while True:  
-    navegador.find_element(By.XPATH,'//*[@id="tabela_wrapper"]/div[3]/div[1]/div/table/thead/tr[2]/th[4]').click()
-    time.sleep(1)
-    navegador.find_element(By.XPATH,'//*[@id="tabela_wrapper"]/div[3]/div[1]/div/table/thead/tr[2]/th[4]').click()
-    time.sleep(1)
-    status = navegador.find_element(By.XPATH,'//*[@id="tabela"]/tbody/tr[1]/td[4]/a').text
-    if status == 'Inativado':
-        navegador.find_element(By.XPATH,'//*[@id="tabela"]/tbody/tr[1]/td[2]/a').click()
-        time.sleep(5)
-        navegador.find_element(By.XPATH,'//*[@id="eapiForm"]/fieldset[1]/div/div/span/small').click()
+    while True:
+        print(1)
+        navegador.find_element(By.XPATH,'//*[@id="tabela_wrapper"]/div[3]/div[1]/div/table/thead/tr[2]/th[4]').click()
         time.sleep(1)
-        navegador.find_element(By.XPATH,'//*[@id="eapiForm"]/fieldset[3]/div[1]/div/span/small').click()
-        time.sleep(1)
-        navegador.find_element(By.XPATH,'//*[@id="eapiForm"]/div/div/div/div/div[1]/a').click()
-        time.sleep(5)
-        navegador.find_element(By.XPATH,'/html/body/div[1]/div/div[3]/div/nav/ul/li[1]/a').click()
-        time.sleep(5)
+        status = navegador.find_element(By.XPATH,'//*[@id="tabela"]/tbody/tr[1]/td[4]/a').text
+        if status == 'Inativado':
+            break    
+
+    navegador.find_element(By.XPATH,'//*[@id="tabela"]/tbody/tr[1]/td[2]/a').click()
+    time.sleep(5)
+    navegador.find_element(By.XPATH,'//*[@id="eapiForm"]/fieldset[1]/div/div/span/small').click()
+    time.sleep(1)
+    navegador.find_element(By.XPATH,'//*[@id="eapiForm"]/fieldset[3]/div[1]/div/span/small').click()
+    time.sleep(1)
+    navegador.find_element(By.XPATH,'//*[@id="eapiForm"]/div/div/div/div/div[1]/a').click()
+    time.sleep(5)
+    navegador.find_element(By.XPATH,'/html/body/div[1]/div/div[3]/div/nav/ul/li[1]/a').click()
+    time.sleep(5)
     
 
